558e98286c3553b3c5f6966750652e8547baef76
LielGold <87268288+c1J2OSAtN9@users.noreply.github.com>
2023-09-24 03:07:46 +0300

    base works

--- App.tsx
+++ App.tsx
@@ -1,9 +1,77 @@
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button} from 'react-native';
+
+import { initialize, requestPermission, readRecords} from 'react-native-health-connect';
 
 export default function App() {
+  const [result, setResult] = useState("readSampleData not called");
+  const [err_log, setErrLog] = useState("err_log not set");
+
+  const readSampleData = async () => {
+    try {
+      setErrLog("before initilization + ");
+
+      // initialize the client
+      const isInitialized = await initialize();
+
+      if(isInitialized){
+        //setErrLog(prv_err => prv_err + "initilization worked + ");
+        setErrLog("initilization worked + ");
+      }
+      else{
+        //setErrLog(prv_err => prv_err + "initilization didn't work + ");
+        setErrLog("initilization didn't work + ");
+      }
+    
+      // request permissions
+      const grantedPermissions = await requestPermission([
+        { accessType: 'read', recordType: 'ActiveCaloriesBurned' },
+      ]);
+
+      if(grantedPermissions.length===0){
+        setErrLog(prv_err => prv_err + "no permission granted + ");
+      }      
+      else{
+        setErrLog(prv_err => prv_err + "permission granted + ");
+      }
+
+    
+      // check if granted  
+      const result = await readRecords('ActiveCaloriesBurned', {
+        timeRangeFilter: {
+          operator: 'between',
+          startTime: '2023-01-09T12:00:00.405Z',
+          endTime: '2023-01-09T23:53:15.405Z',
+        },
+      });
+      // Set the result in the state
+      setResult(JSON.stringify(result, null, 2));      
+    }
+    catch (error) {
+      setResult("error reading data - " + error);
+      setErrLog(prv_err => prv_err + "no data received + ");
+    }
+  };  
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Test</Text>
+      <Button title="Read Sample Data" onPress={readSampleData} />
+      {result && (
+        <Text style={{ marginTop: 10 }}>
+          Result:
+          {result}
+        </Text>
+      )}      
+      {err_log && (
+        <Text style={{ marginTop: 10 }}>
+          error log:
+          {err_log}
+        </Text>
+      )}      
     </View>
   );
 }
@@ -16,3 +84,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
